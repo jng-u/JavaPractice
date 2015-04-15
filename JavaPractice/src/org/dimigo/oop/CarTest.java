f518644dac7c9048f9825d75f46e9dc2ff44a694
HP <HP@HP-PC>
2015-04-15 14:11:12 +0900

    car2

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -19,31 +19,33 @@ public class CarTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Car car = new Car();
+		Car2 car_1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car2 car_2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		Car2 car_3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
 		
-		car.setCompany("현대자동차");
+		/*car.setCompany("현대자동차");
 		car.setModel("제네시스");
 		car.setColor("검정색");
 		car.setMaxSpeed(225);
-		car.setPrice(50000000);
-		Output(car);
+		car.setPrice(50000000);*/
+		Output(car_1);
 
-		car.setCompany("기아자동차");
+		/*car.setCompany("기아자동차");
 		car.setModel("K7");
 		car.setColor("흰색");
 		car.setMaxSpeed(246);
-		car.setPrice(40000000);
-		Output(car);
+		car.setPrice(40000000);*/
+		Output(car_2);
 
-		car.setCompany("삼성자동차");
+		/*car.setCompany("삼성자동차");
 		car.setModel("SM7");
 		car.setColor("회색");
 		car.setMaxSpeed(200);
-		car.setPrice(38000000);
-		Output(car);
+		car.setPrice(38000000);*/
+		Output(car_3);
 	}
 
-	public static void Output(Car car){
+	public static void Output(Car2 car){
 		System.out.println("<<자동차 목록>>");
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
